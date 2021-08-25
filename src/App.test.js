b73f921b520f8645a371ef679f7440c33684fe5d
algo_rhym <jimp802@gmail.com>
2021-08-24 22:01:52 -0400

    fixing test5

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {render} from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from './App';
 
 
 describe("App component", () => {
+
     it("should render app whit error", () => {
     const{asFragment} = render(<App />);
+    
     expect(asFragment()).toMatchSnapshot();
     });
 });
